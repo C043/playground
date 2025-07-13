1dff6f9115c190a6bca9f361dfcc9886faf1270f
Mario Fragnito <mariofragnitoph@gmail.com>
2025-07-13 23:12:06 +0200

    Strings manipulation in python

--- python/manipulateStrings.py
+++ python/manipulateStrings.py
@@ -4,3 +4,24 @@ def replaceWhiteSpaceWithDashes(string: str):
 
 
 print(replaceWhiteSpaceWithDashes("Testing things out a little bit"))
+
+print("(name, date),\n".strip("\n)(,"))
+
+print("i got rejected".endswith("rejected"))
+print("i got rejected".find("rejected"))
+
+x: list = ['"abc"', "dfg", "hlmo", '"opxyz"']
+
+for idx, string in enumerate(x):
+    x[idx] = string.replace('"', "")
+
+print(x)
+
+string: str = "Mississippi"
+
+lastIndexOfP: int = string.rindex("p")
+
+convertedString: list = list(string)
+convertedString[lastIndexOfP] = ""
+newString: str = "".join(convertedString)
+print(newString)
