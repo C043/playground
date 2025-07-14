aff4f01104b62dc996d7e9c295bc8e98d593a765
C043 <mariofragnitoph@gmail.com>
2025-07-14 23:15:08 +0200

    Keeps experimenting with control flow

--- python/conditionals.py
+++ python/conditionals.py
@@ -9,3 +9,33 @@ def removeAllNegatives(ls: list):
 
 
 print(removeAllNegatives(x))
+
+y = [[1, -1, 0], [2, 5, -9], [-2, -3, 0]]
+
+
+def countTotalNumberOfNegativeNumbers(ls: list):
+    count: int = 0
+    for list in ls:
+        for num in list:
+            if num < 0:
+                count += 1
+    return count
+
+
+print(countTotalNumberOfNegativeNumbers(y))
+
+
+def howMuchHighIsTheNum(num: int):
+    if num < -5:
+        print("very low")
+    elif num >= -5 and num <= 0:
+        print("low")
+    elif num == 0:
+        print("neutral")
+    elif num > 0 and num <= 5:
+        print("high")
+    else:
+        print("very high")
+
+
+howMuchHighIsTheNum(50)
