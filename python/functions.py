3e9d23377e31c4c112607ca1471e7f40bdcbaa8e
C043 <mariofragnitoph@gmail.com>
2025-07-22 22:11:26 +0200

    Experimented with decorators

--- python/functions.py
+++ python/functions.py
@@ -1,3 +1,6 @@
+from typing_extensions import clear_overloads
+
+
 x = 5
 
 
@@ -16,3 +19,24 @@ funct_1()
 funct_2()
 
 print(x)
+
+
+# decorators
+def decorate(func):
+    print("in decorate function, decoratind", func.__name__)
+
+    def wrapperFunc(*args):
+        print("Executing", func.__name__)
+        cleaned_args = " ".join(str(arg) for arg in args)
+        string = f"<html>{cleaned_args}</html>"
+        return func(string)
+
+    return wrapperFunc
+
+
+@decorate
+def myfunc(parameter):
+    print(parameter)
+
+
+myfunc("hello")
