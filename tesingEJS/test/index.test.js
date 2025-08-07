b001f7e8af2b62fc3b6297a58112659348926450
C043 <mariofragnitoph@gmail.com>
2025-08-07 18:41:34 +0200

    Adds sample tests for ejs file types

--- tesingEJS/test/index.test.js
+++ tesingEJS/test/index.test.js
@@ -1,21 +1,56 @@
 import { JSDOM } from "jsdom"
 import ejs from "ejs"
+import { dirname } from "path"
 import path from "path"
+import { fileURLToPath } from "url"
+import assert from "assert"
+import { beforeEach, describe, it } from "mocha"
+import sinon from "sinon"
+import { fetch, Response } from "undici"
 
-const targetFile = path.resolve(__dirname, "../views/index.ejs")
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const targetFile = path.resolve(__dirname, "../views/homePage.ejs")
 
 ejs.renderFile(targetFile, function (err, str) {
   if (str) {
-    let dom
-    let container
-
     describe("Home Page", () => {
-      dom = new JSDOM(str)
-      container = dom.window.document.body
-    })
+      let dom
+      let body
+      let fetchStub
 
-    test("h1 should be present and have specific text", () => {
-      console.log(container.querySelector("h1").textContent)
+      beforeEach(() => {
+        dom = new JSDOM(str, { runScripts: "dangerously", resources: "usable" })
+        body = dom.window.document.body
+
+        dom.window.fetch = fetch
+
+        fetchStub = sinon.stub(dom.window, "fetch").resolves(new Response("{}"))
+      })
+
+      it("h1 should be present and have specific text", () => {
+        assert.equal(body.querySelector("h1").textContent, "Home Page")
+      })
+
+      it("Button should be present and call fetch with correct URL on button click", async () => {
+        const button = body.querySelector("#post-button")
+
+        assert.ok(button, "Expected button to exist")
+
+        button.dispatchEvent(
+          new dom.window.MouseEvent("click", { bubbles: true })
+        )
+
+        await new Promise(r => setTimeout(r, 0))
+
+        sinon.assert.calledOnce(fetchStub)
+        sinon.assert.calledWith(
+          fetchStub,
+          "/api/trigger-action",
+          sinon.match.has("method", "POST")
+        )
+      })
     })
   }
 })
