06125cc1166c97df94fc171f6b82123bdc6feedc
C043 <mariofragnitoph@gmail.com>
2025-08-09 00:05:55 +0200

    Adds test to check if specific test is rendered if user is admin or not

--- tesingEJS/server/controllers/controller.js
+++ tesingEJS/server/controllers/controller.js
@@ -1,5 +1,10 @@
 export function renderHomePage(req, res) {
   return res.render("homePage", {
+    jsGlobals: {
+      user: {
+        isAdmin: false
+      }
+    },
     title: "EJS TESTING"
   })
 }
--- tesingEJS/test/index.test.js
+++ tesingEJS/test/index.test.js
@@ -13,44 +13,107 @@ const __dirname = dirname(__filename)
 
 const targetFile = path.resolve(__dirname, "../views/homePage.ejs")
 
-ejs.renderFile(targetFile, function (err, str) {
-  if (str) {
-    describe("Home Page", () => {
+// User admin
+ejs.renderFile(
+  targetFile,
+  {
+    jsGlobals: {
+      user: {
+        isAdmin: true
+      }
+    }
+  },
+  function (err, str) {
+    if (str) {
+      describe("Home Page", () => {
+        let dom
+        let body
+        let fetchStub
+
+        beforeEach(() => {
+          dom = new JSDOM(str, {
+            runScripts: "dangerously",
+            resources: "usable"
+          })
+          body = dom.window.document.body
+
+          dom.window.fetch = fetch
+
+          fetchStub = sinon
+            .stub(dom.window, "fetch")
+            .resolves(new Response("{}"))
+        })
+
+        it("h1 should be present and have specific text", () => {
+          assert.equal(body.querySelector("h1").textContent, "Home Page")
+        })
+
+        it("Button should be present and call fetch with correct URL on button click", async () => {
+          const button = body.querySelector("#post-button")
+
+          assert.ok(button, "Expected button to exist")
+
+          button.dispatchEvent(
+            new dom.window.MouseEvent("click", { bubbles: true })
+          )
+
+          await new Promise(r => setTimeout(r, 0))
+
+          sinon.assert.calledOnce(fetchStub)
+          sinon.assert.calledWith(
+            fetchStub,
+            "/api/trigger-action",
+            sinon.match.has("method", "POST")
+          )
+        })
+
+        it("should show a specific text if user is admin", () => {
+          assert.equal(
+            body.querySelector("#admin-welcome-message").textContent,
+            "Welcome, admin"
+          )
+        })
+      })
+    } else {
+      throw new Error("EJS NOT RENDERED")
+    }
+  }
+)
+
+// User not admin
+ejs.renderFile(
+  targetFile,
+  {
+    jsGlobals: {
+      user: {
+        isAdmin: false
+      }
+    }
+  },
+  function (err, str) {
+    if (str) {
       let dom
       let body
       let fetchStub
 
       beforeEach(() => {
-        dom = new JSDOM(str, { runScripts: "dangerously", resources: "usable" })
+        dom = new JSDOM(str, {
+          runScripts: "dangerously",
+          resources: "usable"
+        })
         body = dom.window.document.body
 
         dom.window.fetch = fetch
 
         fetchStub = sinon.stub(dom.window, "fetch").resolves(new Response("{}"))
       })
-
-      it("h1 should be present and have specific text", () => {
-        assert.equal(body.querySelector("h1").textContent, "Home Page")
+      describe("Home Page", () => {
+        it("should not render specific text if user is not admin", () => {
+          assert.ok(!body.querySelector("#admin-welcome-message"))
+        })
       })
-
-      it("Button should be present and call fetch with correct URL on button click", async () => {
-        const button = body.querySelector("#post-button")
-
-        assert.ok(button, "Expected button to exist")
-
-        button.dispatchEvent(
-          new dom.window.MouseEvent("click", { bubbles: true })
-        )
-
-        await new Promise(r => setTimeout(r, 0))
-
-        sinon.assert.calledOnce(fetchStub)
-        sinon.assert.calledWith(
-          fetchStub,
-          "/api/trigger-action",
-          sinon.match.has("method", "POST")
-        )
-      })
-    })
+    } else {
+      throw new Error("EJS NOT RENDERED")
+    }
   }
-})
+)
