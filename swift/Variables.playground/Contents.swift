cfa25500e18fbc7dca679bf7d42dbce13b0c1099
Mario Fragnito <mariofragnitoph@gmail.com>
2025-09-25 18:20:01 +0200

    Adds variables swift playground

---
+++ swift/Variables.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+// type hinting
+// let is the way to create constants (const in JavaScript)
+let numberOfStopLights: Int = 4
+
+var population: Int
+population = 5422
+let townName: String = "Night City"
+let townElevation: Int = 5000
+let townDescription: String = "\(townName) has a population of \(population) and \(numberOfStopLights) stoplights. The has a \(townElevation) meters elevation on the sea level"
+print(townDescription)
