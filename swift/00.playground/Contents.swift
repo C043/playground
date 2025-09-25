c306e5d46155f2191b9de929711e3a36b2a37c32
Mario Fragnito <mariofragnitoph@gmail.com>
2025-09-25 17:50:12 +0200

    Adds xuserdata to gitignore

--- swift/00.playground/Contents.swift
+++ swift/00.playground/Contents.swift
@@ -1,3 +1,8 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+var str = "Hello, playground"
+str += "!"
+// Prints the str variable in the console
+print(str)
+var username = "C043"
+print(username)
