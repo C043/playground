a97a4693b2009b3b208b106d2734add080b0bba9
Mario Fragnito <mariofragnitoph@gmail.com>
2025-09-26 10:24:26 +0200

    Adds Switch swift playground

---
+++ swift/Switch.playground/Contents.swift
@@ -0,0 +1,41 @@
+import Cocoa
+
+var statusCode: Int = 418
+var errorString: String = "The request failed: "
+
+switch statusCode {
+case 401:
+    errorString += "Unauthorized"
+case 400...417:
+    errorString += "Client error, \(statusCode)"
+case 500...505:
+    errorString += "Server error, \(statusCode)"
+case let code where code < 100 || code > 600:
+    errorString = "\(code) is an illegal status code."
+default:
+    errorString = "Unexpected error encountered."
+}
+
+let error = (code: statusCode, msg: errorString)
+error.code
+error.msg
+
+let firstErrorCode = 404
+let secondErrorCode = 418
+let errorCodes = (firstErrorCode, secondErrorCode)
+
+switch errorCodes {
+case (404, 404):
+    print("Both error codes were 404.")
+case (404, _):
+    print("Only the 1st code is 404, and we don't care about the 2nd code.")
+case (_, 404):
+    print("Only the 2nd code is 404, and we don't care about the 1st code.")
+default:
+    print("Neighter code is 404.")
+}
+
+let age = 26
+if case 18...35 = age, age >= 25, age < 30 {
+    print("Cool demographic, can rent a car and not in their thirties")
+}
