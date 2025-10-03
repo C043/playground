1a702bed6265d38ae4c2e07882c038c79be4c08e
Mario Fragnito <mariofragnitoph@gmail.com>
2025-10-03 11:06:40 +0200

    Adds Dictionaries Swift playground

--- swift/Dictionaries.playground/Contents.swift
+++ swift/Dictionaries.playground/Contents.swift
@@ -33,3 +33,21 @@ for movie in movieRatings.keys {
 
 // Translating a Dictionary to an Array
 let watchedMovies = Array(movieRatings.keys)
+
+// Silver Challenge
+var teams = ["Red Pandas": ["Burb", "Testich", "Probatonich","Rubudubdub", "Andrea"], "Black Dragons": ["Sylvanovich", "Bolas", "Smaug", "Red", "Breach"], "The Marvelous": ["Ms. Marvel", "Cap", "Iron Man", "Spiderman", "Antman"]]
+
+var players: [String] = []
+for teamPlayers in teams.values {
+    players += teamPlayers
+}
+print("The NWSL has the following players: \(players)")
+
+// Gold Challenge
+for (team, teamPlayers) in teams {
+    print("\(team):")
+    for i in 0...teamPlayers.count - 1 {
+        print(teamPlayers[i])
+    }
+    print("\n")
+}
