5df68114c459ed10477aff4e73ccf94ad536b90b
Mario Fragnito <mariofragnitoph@gmail.com>
2025-10-15 01:03:37 +0200

    Adds Enumerations Swift Playground

--- swift/Enumerations.playground/Contents.swift
+++ swift/Enumerations.playground/Contents.swift
@@ -24,3 +24,87 @@ case .center:
 case .justify:
     print("Justify")
 }
+
+// Methods
+enum LightBulb {
+    case on
+    case off
+
+    func surfaceTemperature(forAmbientTemperature ambient: Double) -> Double {
+        switch self {
+        case .on:
+            return ambient + 150.0
+        case .off:
+            return ambient
+        }
+    }
+
+    mutating func toggle() {
+        switch self {
+        case .on:
+            self = .off
+        case .off:
+            self = .on
+        }
+    }
+}
+
+var bulb = LightBulb.on
+let ambientTemperature = 77.0
+var bulbTemerature = bulb.surfaceTemperature(
+    forAmbientTemperature: ambientTemperature
+)
+
+bulb.toggle()
+bulbTemerature = bulb.surfaceTemperature(
+    forAmbientTemperature: ambientTemperature
+)
+
+// Associated Values
+enum ShapeDimensions {
+    // point has no associated value - it is dimensionless
+    case point
+
+    // Square's associated value is the length of on side
+    case square(side: Double)
+
+    // rectangle's associated value defines its width and height
+    case rectangle(width: Double, height: Double)
+
+    case rightTriangle(base: Double, heigth: Double, cateto: Double)
+
+    func area() -> Double {
+        switch self {
+        case .point:
+            return 0
+        case .square(let side):
+            return side * side
+
+        case .rectangle(width: let w, height: let h):
+            return w * h
+        case .rightTriangle(base: let b, heigth: let h, cateto: let c):
+            return b * h / 2
+        }
+    }
+
+    func perimeter() -> Double {
+        switch self {
+        case .point:
+            return 0
+        case .square(let side):
+            return side * 4
+        case .rectangle(width: let w, height: let h):
+            return w * 2 + h * 2
+        case .rightTriangle(base: let b, heigth: let h, cateto: let c):
+            return b + h + c
+        }
+    }
+}
+
+var squareShape = ShapeDimensions.square(side: 10.0)
+var rectShape = ShapeDimensions.rectangle(width: 5.0, height: 10.0)
+
+let squareArea = squareShape.area()
+let rectArea = rectShape.area()
+
+let rectPerimeter = rectShape.perimeter()
