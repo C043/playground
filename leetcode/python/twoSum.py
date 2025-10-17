0017361ae57ddeb2ba504bf96813be7d5dc89672
C043 <mariofragnitoph@gmail.com>
2025-10-17 15:04:36 +0200

    Adds twoSum python solution optimized

--- leetcode/python/twoSum.py
+++ leetcode/python/twoSum.py
@@ -1,32 +1,3 @@
-# @leet imports start
-from bisect import *
-from collections import *
-from copy import *
-from datetime import *
-from heapq import *
-from math import *
-from re import *
-from string import *
-from random import *
-from itertools import *
-from functools import *
-from operator import *
-import string
-import re
-import datetime
-import collections
-import heapq
-import bisect
-import copy
-import math
-import random
-import itertools
-import functools
-import operator
-
-# @leet imports end
-
-
 # @leet start
 class Solution(object):
     def twoSum(self, nums, target):
@@ -40,5 +11,30 @@ class Solution(object):
                 if a + b == target:
                     return [i, y]
 
+
+# @leet end
+
+
+# @leet start
+class SolutionOptimized(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        visited = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in visited:
+                return [visited[complement], i]
+
+            visited[num] = i
+
+
+solution = SolutionOptimized()
+print(solution.twoSum([1, 3, 5], 8))
 
 # @leet end
