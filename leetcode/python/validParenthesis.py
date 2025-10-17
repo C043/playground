ebc0416645f66b274d622cba785d7f7c81c88f97
C043 <mariofragnitoph@gmail.com>
2025-10-17 18:18:57 +0200

    Adds validParenthesis.py description

--- leetcode/python/validParenthesis.py
+++ leetcode/python/validParenthesis.py
@@ -26,3 +26,13 @@ solution = Solution()
 print(solution.isValid("([])"))
 
 # @leet end
+
+"""
+This implementation is O(n) in time and space complexity because it loops over all the parenthesis in the string and it possibly saves them all in the stack if there are opened parenthesis only.
+
+We define a dict with key closed parenthesis and value its opened counterpart.
+We start a stack
+We loop over all the chars in the string and if the char is opened, we add it to the stack, if it's closed, we check the last added parenthesis in the stack, if they don't match, we return false.
+
+At the end we return whether if the stack is empty or not, if it's empty, it means that the parenthesis are valid.
+"""
