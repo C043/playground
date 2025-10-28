6ae721f991f0d2a25035f041b87a00a603abb125
Mario Fragnito <mariofragnitoph@gmail.com>
2025-10-28 10:57:13 +0100

    Adds silver challenge for swift monster town playground

---
+++ swift/MonsterTownChallenges/MonsterTown/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Mario Fragnito on 15/10/25.
+//
+
+import Foundation
+
+var myTown = Town()
+
+myTown.changePopulation(by: 2)
+
+let fredTheZombie = Zombie()
+fredTheZombie.town = myTown
+fredTheZombie.terrorizeTown()
+fredTheZombie.town?.printDescription()
