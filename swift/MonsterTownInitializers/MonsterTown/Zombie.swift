f60c4ab644ef82aa71fae817c9f6d482ec7740de
Mario Fragnito <mariofragnitoph@gmail.com>
2025-11-03 10:26:48 +0100

    Adds silver challenge to the initialization chapter

--- swift/MonsterTownInitializers/MonsterTown/Zombie.swift
+++ swift/MonsterTownInitializers/MonsterTown/Zombie.swift
@@ -15,29 +15,43 @@ class Zombie: Monster {
     var walksWithLimp: Bool
     private(set) var isFallingApart: Bool
 
-    init(limp: Bool, fallingApart: Bool, town: Town?, monsterName: String) {
-        walksWithLimp = limp
-        isFallingApart = fallingApart
+    required init(town: Town?, monsterName: String) {
         super.init(town: town, monsterName: monsterName)
     }
-    
-    convenience init(limp: Bool, fallingApart: Bool){
-        self.init(limp: limp, fallingApart: fallingApart, town: nil, monsterName: "Fred")
-        if walksWithLimp{
+
+    convenience init(
+        limp: Bool,
+        fallingApart: Bool,
+        town: Town?,
+        monsterName: String
+    ) {
+        walksWithLimp = limp
+        isFallingApart = fallingApart
+        self.init(town: town, monsterName: monsterName)
+    }
+
+    convenience init(limp: Bool, fallingApart: Bool) {
+        self.init(
+            limp: limp,
+            fallingApart: fallingApart,
+            town: nil,
+            monsterName: "Fred"
+        )
+        if walksWithLimp {
             print("This zombie has a bad knee.")
         }
     }
-    
+
     required init(town: Town?, monsterName: String) {
         walksWithLimp = false
-        isFallingApart=false
-        super.init(town:town, monsterName: monsterName)
+        isFallingApart = false
+        super.init(town: town, monsterName: monsterName)
     }
-    
+
     deinit {
         print("Zombie \(name) is no longer with us.")
     }
-    
+
     func regenrate() {
         walksWithLimp = false
     }
