c4c4966189ad96f6d1fe0f1fdc222aadcf3ae276
C043 <mariofragnitoph@gmail.com>
2025-11-06 19:44:45 +0100

    WIP Adds pacificAtlanticWaterFlow.py

---
+++ leetcode/python/pacificAtlanticWaterFlow.py
@@ -0,0 +1,88 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights or not heights[0]:
+            return []
+
+        ROWS, COLS = len(heights), len(heights[0])
+
+        pacificSet = set()
+        atlanticSet = set()
+
+        pacificQueue = deque()
+        atlanticQueue = deque()
+
+        for r in range(ROWS):
+            # Left border
+            pacificQueue.append((r, 0))
+            pacificSet.add((r, 0))
+
+            # Right border
+            atlanticQueue.append((r, COLS - 1))
+            atlanticSet.add((r, COLS - 1))
+
+        for c in range(COLS):
+            # Top border
+            pacificQueue.append((0, c))
+            pacificSet.add((0, c))
+
+            # Bottom border
+            atlanticSet.add((ROWS - 1, c))
+            atlanticQueue.append((ROWS - 1, c))
+
+        def bfs(queue: deque, reachableCellsSet: set):
+            while queue:
+                currentRow, currentCol = queue.popleft()
+
+                for deltaRow, deltaCol in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    neighborRow, neighborCol = (
+                        currentRow + deltaRow,
+                        currentCol + deltaCol,
+                    )
+
+                    if (
+                        0 <= neighborRow < ROWS
+                        and 0 <= neighborCol < COLS
+                        and (neighborRow, neighborCol) not in reachableCellsSet
+                        and heights[neighborRow][neighborCol]
+                        >= heights[currentRow][currentCol]
+                    ):
+                        reachableCellsSet.add((neighborRow, neighborCol))
+                        queue.append((neighborRow, neighborCol))
+
+        bfs(pacificQueue, pacificSet)
+        bfs(atlanticQueue, atlanticSet)
+
+        result = list(pacificSet.intersection(atlanticSet))
+
+        return result
+
+
+solution = Solution()
+print(
+    solution.pacificAtlantic(
+        [
+            [1, 2, 2, 3, 5],
+            [3, 2, 3, 4, 4],
+            [2, 4, 5, 3, 1],
+            [6, 7, 1, 4, 5],
+            [5, 1, 1, 2, 4],
+        ]
+    )
+)
+
+"""
+The main concept of this algorithm is that we trace back from the cells touch the oceans creating two separated sets that contains all the cells in the path from the ocean to the rest of the graph.
+We do this using populating two separated sets and queues with the cells from each border (because those cells surely touch the oceans).
+Then we perform the BFS graph traversal adding to the queue and to the set only the neighbor neighbors cells that are inside the graph constraints, we did not visit yet and that have their height more or equal to the current cell.
+This ensures that the water can drain through those cells because the current cell is lower than the neighbors.
+
+Once we traversed and populated for both oceans, we return the intersection of the two sets because those cells are the complete paths that the water can traverse to reach both oceans. It's kind of smart!
+
+The time complexity is O(R * C)
+The space complexity is O(R * C)
+TODO: Reflect on this algorithm more
+"""
