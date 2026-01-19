a1b81d65006f863a1669e988148877828402ac66
Mario Fragnito <mariofragnitoph@gmail.com>
2026-01-19 21:22:55 +0100

    WIP combinationSum.py

--- leetcode/python/combinationSum.py
+++ leetcode/python/combinationSum.py
@@ -13,7 +13,6 @@ class Solution:
         ):
             if remainingTarget == 0:
                 result.append(path.copy())
-                return result
 
             for startingIndex in range(startingIndex, len(candidates)):
                 if candidates[startingIndex] > remainingTarget:
@@ -24,14 +23,23 @@ class Solution:
                 )
                 path.pop()
 
-            return result
-
-        return backtrack(0, target, [])
+        backtrack(0, target, [])
+        return result
 
 
 solution = Solution()
 print(solution.combinationSum([2, 3, 6, 7], 7))
 
 """
-
+This implementation uses a backtrack recursive algorithm.
+The helper recursive function takes three arguments: the starting index to check in the candidates array, the remaining target and the path.
+We need the starting index because we can reuse candidates but we cannot go backwards in checking candidates. Once we checked a candidate enough, we can only go forward. 
+We need the remaining target as we're going to pass the remaining target - the current candidate to the backtrack algorithm.
+The path is the current list of candidates we're checking to see if they work. If they work, we'll append a copy of the path to the result list.
+So first of all we sort the candidates list. This helps a lot because it permits us to not check all candidates that are bigger than the target.
+Then we initiate an empty result list that we're going to return in the end.
+Then we define the backtrack recursive function with base case remaining target == 0 (this means that the current path is a possible result and we add a copy of it to the result list).
+Then we start a loop from the starting index that is going to be 0 the first time until we loop over all the candidates list.
+If the candidate is bigger than the remaining target, we break out of the loop because we can stop looking, from there on, the candidates are all too big.
+We append the candidate to the current path and we call the backtrack function again with the current index and the remaining target minus the current candidate and the current path. We do this because we used the first candidate, then we pop from the path.
 """
