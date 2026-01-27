21d45c68244c98512ac3c9fb84462486de142c2f
C043 <mariofragnitoph@gmail.com>
2026-01-27 18:54:46 +0100

    Adds minStack.py

--- leetcode/python/minStack.py
+++ leetcode/python/minStack.py
@@ -44,3 +44,13 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.minStack[-1]
+
+
+"""
+This implementation is fairly simple.
+The main idea is to keep a list beside the main stack list in which we always append the minimum between the new value and the current min value. This way, when we pop, we pop from both stacks to keep them aligned. The self.minStack will always have the minimum value from the main stack as its last element.
+When we want to check the min value in the stack, we just need to look at the last element in the min list we're keeping.
+
+This implementation is O(1) time complexity for all the operations.
+This implementation is O(n) space complexity because even if it stores data two times, once for the self.stack list and once for the self.minStack list, the space grows linearly with the input growth.
+"""
