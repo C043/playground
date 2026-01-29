4790112e444118f9fd18f95bfc77cbf9a4d01701
C043 <mariofragnitoph@gmail.com>
2026-01-29 16:16:06 +0100

    Adds numberOfEquaivalentDominoPairs.py

--- leetcode/python/numberOfEquivalentDominoPairs.py
+++ leetcode/python/numberOfEquivalentDominoPairs.py
@@ -17,21 +17,23 @@ class Solution:
         return result
 
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        dominoMap = dict()
-        result = 0
-        for domino in dominoes:
-            sortedList = sorted(domino)
-            key = tuple(sortedList)
-            if key in dominoMap:
-                dominoMap[key] += 1
-                result += 1
-            else:
-                dominoMap[key] = 1
+        counts = dict()
+        for a, b in dominoes:
+            key = (a, b) if a >= b else (b, a)
+            counts[key] = counts.get(key, 0) + 1
 
-        return result
+        return sum(c * (c - 1) // 2 for c in counts.values())
 
 
 dominoes = [[1, 2], [2, 1], [3, 4], [5, 6]]
 dominoes = [[1, 2], [1, 2], [1, 1], [1, 2], [2, 2]]
 solution = Solution()
 print(solution.numEquivDominoPairs(dominoes))
+
+"""
+The main idea behind this implementation is that we count how many times every domino is present in the input conforming its key so that every compatible domino is saved with the same key.
+Then we sum the pairs and we return them.
+
+The time complexity of the optimized solution is O(n) where n is the number of dominoes in the input
+The space complexity of the same solution is O(n) in the worst case because we store each domino as a key in a map (we save its count in the map, but in the worst case that means we'll have a key value for each domino in the input)
+"""
