4e0981a12cc2e99a6d643166da8978b910773759
C043 <mariofragnitoph@gmail.com>
2026-02-02 11:05:30 +0100

    Adds pathSum.py

--- leetcode/python/pathSum.py
+++ leetcode/python/pathSum.py
@@ -37,3 +37,14 @@ root = TreeNode(
 root = TreeNode(-2, None, TreeNode(-3))
 solution = Solution()
 print(solution.hasPathSum(root, -5))
+
+"""
+This implementation uses DFS to find the leaves and see if the target sum is 0 after subtracting the root value.
+It is recursive. We call the method on root left and on root right with passing the root left and root right and the target sum minus the current root value.
+We assign the result of these recursive calls to two boolean variables and we return true if any one of these is true. Otherwise we return false.
+The recursive base case is that the node is a leaf and that target sum is 0 after subtructing the current node value.
+
+The time complexity is O(n) because we visit each node exactly once
+The space complexity is O(1) if we're not counting the call stack
+If we're counting the call stack, then it's O(n) too because there will be a call for each node in the tree
+"""
