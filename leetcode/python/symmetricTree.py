d2d97c379447d2af225db2769ab61ed34f6ae13d
C043 <mariofragnitoph@gmail.com>
2026-02-03 08:46:57 +0100

    Adds symmetricTree.py

--- leetcode/python/symmetricTree.py
+++ leetcode/python/symmetricTree.py
@@ -18,21 +18,18 @@ class Solution:
         return self.check(root.left, root.right)
 
     def check(self, left: Optional[TreeNode], right: Optional[TreeNode]) -> bool:
-        if not left and not right:
-            return True
-        elif not left and right or not right and left:
+        leftTraversal = True
+        rightTraversal = True
+
+        if not left and right or not right and left:
             return False
         elif right and left and right.val != left.val:
             return False
-
-        leftTraversal = False
-        rightTraversal = False
-        if left and left.left and right and right.right:
+        elif left and right and left.val == right.val:
             leftTraversal = self.check(left.left, right.right)
-        if left and left.right and right and right.left:
             rightTraversal = self.check(left.right, right.left)
 
-        return leftTraversal or rightTraversal
+        return leftTraversal and rightTraversal
 
 
 root = TreeNode(
@@ -40,9 +37,18 @@ root = TreeNode(
     TreeNode(2, TreeNode(4, None, TreeNode(1))),
     TreeNode(2, None, TreeNode(1, TreeNode(2))),
 )
-root = TreeNode(1, TreeNode(2, None, TreeNode(3)), TreeNode(2, None, TreeNode(3)))
 root = TreeNode(
     1, TreeNode(2, TreeNode(3), TreeNode(4)), TreeNode(2, TreeNode(4), TreeNode(3))
 )
+root = TreeNode(1, TreeNode(2, None, TreeNode(3)), TreeNode(2, None, TreeNode(3)))
 solution = Solution()
 print(solution.isSymmetric(root))
+
+"""
+This implementation uses recursive DFS in order to check the symmetry of a binary tree.
+We create a helper method that checks if two nodes have the same value. If so, it calls itself on left.left with right.right and with left.right with right.left to check the keep checking the symmetry down the tree.
+If any one of these return False, it means that the tree is not symmetric and we return false.
+
+The time complexity is O(n) where n the number of nodes in the tree (we visit them all once in the worst case)
+The space complexity is O(h) where h is the height of the tree
+"""
