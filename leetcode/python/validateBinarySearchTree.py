01add8040a4a3cbf6e9b491f5ee6554f217a8fe5
C043 <mariofragnitoph@gmail.com>
2026-02-05 08:20:04 +0100

    Adds validateBinarySearchTree.py

--- leetcode/python/validateBinarySearchTree.py
+++ leetcode/python/validateBinarySearchTree.py
@@ -38,3 +38,22 @@ root = TreeNode(
 )
 solution = Solution()
 print(solution.isValidBST(root))
+
+"""
+This implementation uses recursive DFS to check at every node if the node is within the low and high constraints.
+The constraints change dynamically based on whether we're going left or right.
+If we're going left, the new high constraint is the current node value.
+This is because when we left subtrees cannot be higher than any node on upper level on their right.
+If we're going right, the new low constraint is the current node value.
+This is because when we right subtrees cannot be lower than any node on upper level on their left.
+
+Constraints are kept between function calls to preserve the right constraints at every call and update the ones that needs updating.
+
+The base recursive case is that we pass None to the function.
+When this happens, we return True because it means that we reached the end of the left or right subtree and it's valid until this point.
+
+At the end, if every recursive call returns True, tree is a valid BST.
+
+The time complexity is O(n) because we visit each node once
+The space complexity is O(h) where h is the height of the tree due to the recursion nature of the algorithm
+"""
