7e67a1b220288b668beea8261610adb199922280
C043 <mariofragnitoph@gmail.com>
2026-02-05 15:56:46 +0100

    Adds constructBinaryTreeFromPreorderAndInorderTraversal.py

--- leetcode/python/constructBinaryTreeFromPreorderAndInorderTraversal.py
+++ leetcode/python/constructBinaryTreeFromPreorderAndInorderTraversal.py
@@ -10,7 +10,7 @@ class TreeNode:
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder and not inorder:
+        if not preorder or not inorder:
             return None
 
         rootIndex = inorder.index(preorder[0])
@@ -34,3 +34,21 @@ preorder = [3, 9, 20, 15, 7]
 inorder = [9, 3, 15, 20, 7]
 solution = Solution()
 print(solution.buildTree(preorder, inorder))
+
+"""
+In this problem we need to construct a binary tree using its postorder and inorder array.
+postorder = [root, left, right]
+inorder = [left, root, right]
+
+We use a recursive DFS function to solve this problem.
+The base idea is to return each node and calculate its left and its right by calling the same function with preorder and postorder array for the left subtree and for the right one.
+
+We calculate them by finding the root in the inorder subtree.
+We know that the root is the first element in the postorder one.
+Then we know the length of the left and right subtrees because they are the elements to the left and to the right of the root in the inorder array.
+So we can use it to separate the left from the right subree in the postorder too.
+We create a TreeNode with the value from the root, recursion with the left postorder and inorder for the left node, and the same thing for the right but with right postorder and inorder arrays.
+
+We return this node (that will be the root of the tree in the end).
+The recursive base case is that there are no elements in the preorder or in the inorder array. In that case we're return None because that will mean that we arrived to a leaf.
+"""
