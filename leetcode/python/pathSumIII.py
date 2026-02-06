776324ae729f8ad844f7bac6faf3fb93496c5f17
C043 <mariofragnitoph@gmail.com>
2026-02-06 16:23:27 +0100

    WIP pathSumIII.py

---
+++ leetcode/python/pathSumIII.py
@@ -0,0 +1,42 @@
+from typing import List, Optional
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        if not root:
+            return 0
+        if targetSum - root.val == 0:
+            return 1
+
+        return count
+    def helper(self, root: Optional[TreeNode], targetSum: int, path: deque[int], count: int) -> int:
+
+
+
+root = TreeNode(
+    5,
+    TreeNode(4, TreeNode(11, TreeNode(7), TreeNode(2))),
+    TreeNode(8, TreeNode(13), TreeNode(4, TreeNode(5), TreeNode(1))),
+)
+root = TreeNode(
+    10,
+    TreeNode(5, TreeNode(3, TreeNode(3), TreeNode(-2)), TreeNode(2, None, TreeNode(1))),
+    TreeNode(-3, None, TreeNode(11)),
+)
+solution = Solution()
+print(solution.pathSum(root, 8))
+#        5
+#      /   \
+#     4     8
+#    /     / \
+#   11    13  4
+#  / \       / \
+# 7   2     5   1
