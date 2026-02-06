1b9ff90f70c37bc4712eb2dfb528dca7c47ed74f
C043 <mariofragnitoph@gmail.com>
2026-02-06 08:19:25 +0100

    Adds sumRootToLeafNumbers.py

--- leetcode/python/sumRootToLeafNumbers.py
+++ leetcode/python/sumRootToLeafNumbers.py
@@ -35,3 +35,18 @@ root = TreeNode(4, TreeNode(9, TreeNode(5), TreeNode(1)), TreeNode(0))
 root = TreeNode(1, TreeNode(2), TreeNode(3))
 solution = Solution()
 print(solution.sumNumbers(root))
+
+"""
+This solution uses a recursive DFS helper function to compile the numbers to sum.
+The recursive base cases are two:
+- There is no node: in that case, we return 0
+- We arrived to a leafe: we return the compiled number
+
+Otherwise we compile the number and we call the helper function on the left and right node with the new compiled number, summing the results.
+
+This compile every number from the root to every leaf and sums them.
+
+The time complexity is O(n) as we traverse all the tree once
+The space complexity is O(1) if we do not count the call stack
+If we count the call stack, then it's O(h) where h is the height of the tree (O(n) on a skewed tree)
+"""
