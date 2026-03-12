b20b8ccfb89d5d8f31bb14d85d9c8f616668fa14
C043 <mariofragnitoph@gmail.com>
2026-03-12 11:52:46 +0100

    Adds logic to store and retrieve bytes in the .ugit/objects directory

--- ugit/ugit/data.py
+++ ugit/ugit/data.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 
 GIT_DIR = ".ugit"
@@ -5,3 +6,17 @@ GIT_DIR = ".ugit"
 
 def init():
     os.makedirs(GIT_DIR)
+    os.makedirs(f"{GIT_DIR}/objects")
+
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f"{GIT_DIR}/objects/{oid}", "wb") as out:
+        out.write(data)
+
+    return oid
+
+
+def get_object(oid):
+    with open(f"{GIT_DIR}/objects/{oid}", "rb") as f:
+        return f.read()
